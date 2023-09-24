bcd46a1bab75445abb75ee315f79eefe5a1c5874
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2023-09-24 18:12:21 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+vel = 45
+
+console.log(`A Velocidade do seu carro é ${vel} KM/H`)
+
+if (vel > 60) { //condição simples
+    console.log(`Você utrapassou o limite de velocidade! MULTADO!`)
+
+}
+console.log('Diriga sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log (`Vivendo no país ${pais}`)
+if (pais == 'Brasil') {
+    console.log ('Você é Brasileiro!')
+} else {
+    console.log ("you're foreigner")
+}
