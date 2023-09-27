930736fda0817f0376c057e45d2ab47399c1e35b
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2023-09-26 22:21:06 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,12 @@
+const num = [5 , 8, 4, 9 , 2]
+
+//console.log(`nosso vetor tem ${num.length} posições`)
+
+/*for(let pos = 0 ; pos < num.length ; pos = pos + 1 ){
+    console.log(num[pos])
+}*/
+
+for(let pos in num ){
+    console.log(num[pos])
+}
+
